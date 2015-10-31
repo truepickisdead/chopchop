befa7eedd3629b14525845b633eda2cbc6d99a2c
Dmitry Antonov <chelog@octothorp.team>
2015-11-01 01:48:04 +0300

    some translation fixes just don't mind :laughing:

--- gamemode/lang/eng.lua
+++ gamemode/lang/eng.lua
@@ -1,4 +1,4 @@
 translate.welcome = "Language successefully loaded."
 
-translate.msgPlyConnect = "{1} gonna join us"
-translate.msgPlyDisconnect = "{1} left us"
+translate.msgPlyConnect = "{1} gonna join us!"
+translate.msgPlyDisconnect = "{1} left"
--- gamemode/lang/rus.lua
+++ gamemode/lang/rus.lua
@@ -1,1 +1,4 @@
 translate.welcome = "Язык успешно загружен."
+
+translate.msgPlyConnect = "К нам скоро зайдет {1}!"
+translate.msgPlyDisconnect = "{1} ушел"
