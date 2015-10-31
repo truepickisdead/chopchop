137398ceb9a252ae92fa72b4dec6d88514100940
Dmitry Antonov <chelog@octothorp.team>
2015-10-31 23:46:27 +0300

    set player hands on spawn just for ^c_ models

--- gamemode/server/player.lua
+++ gamemode/server/player.lua
@@ -23,3 +23,18 @@ function GM:PlayerSpawn( ply )
 	-- loadout player
 	self:PlayerLoadout( ply )
 end
+
+function GM:PlayerSetHandsModel( ply, ent )
+	local simplemodel = player_manager.TranslateToPlayerModelName( ply:GetModel() )
+	local info = player_manager.TranslatePlayerHands( simplemodel )
+	if ( info ) then
+		ent:SetModel( info.model )
+		ent:SetSkin( info.skin )
+		ent:SetBodyGroups( info.body )
+	end
+end
+
+-- =======================
+-- DISABLED BASE FUNCTIONS
+-- =======================
+function GM:PlayerDeathSound() return true end
